fab8a8ab8016a63facd26b539627468454f72163
TEKsafi <135796010+TEKsafi@users.noreply.github.com>
2026-09-15 12:55:52 +0000

    Fix regex string syntax and notifications API version mismatch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.safari.momo_tracker"
-    compileSdk = 37
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
