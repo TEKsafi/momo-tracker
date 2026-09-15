b2a58f00e9c80b675e7605315b9827292bee13f6
TEKsafi <135796010+TEKsafi@users.noreply.github.com>
2026-09-15 06:55:24 +0000

    Patch telephony plugin namespace directly

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,4 +22,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-subprojects { afterEvaluate { extensions.findByType<com.android.build.gradle.LibraryExtension>()?.apply { if (namespace == null) { namespace = group.toString() } } } }
